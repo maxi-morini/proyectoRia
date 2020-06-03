380b187fcacc78f7bb911f90a42805629d327b07
Dreamangst <epm.nicolas@gmail.com>
2020-06-02 21:43:41 -0300

    Estructura basica.

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+
+class App extends React.Component {
+  
+  render() {
+
+
+    return (
+        <span>test</span>
+    );
+    
+  }
+
+}
+
+export default App;
