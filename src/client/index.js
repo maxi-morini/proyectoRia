9b40d0e004ae3df7a9541f01ae92350aaee466fd
Dreamangst <epm.nicolas@gmail.com>
2020-07-01 11:59:46 -0300

    Instalo redux, agrego el Provider  y comprobe que funcione en la creacion del juego. No pude con el ruteo a /Crear, al probar lo hice poniendo el componenete en Home.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,25 +1,34 @@
-import React, {Component} from 'react';
-import{render} from 'react-dom';
-import {BrowserRouter} from 'react-router-dom'
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 
 // Redux
-//import { Provider } from "react-redux";
-//import store from "./client/redux/store";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
+const root = (
+	<React.StrictMode>
+		<Provider store={store}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</Provider>
+	</React.StrictMode>
+);
+render(root, document.getElementById('app'));
 // <React.StrictMode> Para que sea mas estricto con warnings y errores.
 /*
 const Root = (
 	<React.StrictMode>
-		<Provider store={store}> 
+		<Provider store={store}>
 			<App />
 		</Provider>
 	</React.StrictMode>
 );
-
 ReactDOM.render(Root, document.getElementById('root'));
 */
 
 //
 //render(<BrowserRouter><App/></BrowserRouter>, document.getElementById('app'));
-render(<BrowserRouter><App/></BrowserRouter>, document.getElementById('app'));
+//render(<BrowserRouter><App/></BrowserRouter>, document.getElementById('app'));
