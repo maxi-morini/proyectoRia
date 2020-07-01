50cc32f9cbb84962828f03d3c81dce5f1463e981
Dreamangst <epm.nicolas@gmail.com>
2020-07-01 02:53:12 -0300

    Comentarios con lo necesario para mandar el stode de redux a lo largo de la aplicacion. Lo dejo asi ya que no lo probe e instale aun.

--- src/client/index.js
+++ src/client/index.js
@@ -3,5 +3,23 @@ import{render} from 'react-dom';
 import {BrowserRouter} from 'react-router-dom'
 import App from './App'
 
+// Redux
+//import { Provider } from "react-redux";
+//import store from "./client/redux/store";
 
+// <React.StrictMode> Para que sea mas estricto con warnings y errores.
+/*
+const Root = (
+	<React.StrictMode>
+		<Provider store={store}> 
+			<App />
+		</Provider>
+	</React.StrictMode>
+);
+
+ReactDOM.render(Root, document.getElementById('root'));
+*/
+
+//
+//render(<BrowserRouter><App/></BrowserRouter>, document.getElementById('app'));
 render(<BrowserRouter><App/></BrowserRouter>, document.getElementById('app'));
