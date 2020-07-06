5a4175cb662f929f41113ff30cd1509a99c99e21
Dreamangst <epm.nicolas@gmail.com>
2020-07-06 11:07:53 -0300

    Ruteo de Crear a GameCreation

--- src/client/Routes.js
+++ src/client/Routes.js
@@ -5,7 +5,7 @@ import Jugar from './Jugar'
 import LogIn from './LogIn'
 import LogInRegistro from './LoginRegistro'
 import Panel from './Panel'
-import Crear from './Crear'
+import GameCreation from './GameCreation/GameCreation.js'
 import Iniciar from './Jugar/Iniciar'
 import Iniciado from './Jugar/Iniciado'
 const Routes = () => {
@@ -15,7 +15,7 @@ const Routes = () => {
                 {/* <Route exact path="/jugar" component={Jugar}/> */}
                 <Route exact path="/jugar" render={(props)=>(<Jugar {...props} isAuthed={true} text={"GameQuiz (Jugador)"}/>)}/>
                 <Route exact path="/login" component={LogIn}/>
-                <Route exact path="/crear" component={Crear}/>
+                <Route exact path="/crear" component={GameCreation}/>
                 <Route exact path="/registrar" component={LogInRegistro}/>
                 <Route  path="/panel:usr" component={Panel}/>
                 <Route  path="/iniciar:nombre" component={Iniciar}/>
